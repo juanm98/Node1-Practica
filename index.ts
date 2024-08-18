d893c0367b464c705e8ff2fc33c17554452a105c
juanm98 <juanmgarcia098@gmail.com>
2024-08-17 21:37:21 -0400

    Chane type for interface and add .get

--- index.ts
+++ index.ts
@@ -17,4 +17,9 @@ let contacts: Contact [] = [
     {id: 2, fullName: "Maria Jose", phoneNumber: "809-646-5345", email: "mariajose@hotmail.com"}
 ];
 
+// Obtiene los datos de contactos
+app.get(`/api/contacts`, (req: Request, res: Response) => {
+    res.json(contacts);
+});
+
 app.listen(port, () => console.log(`This server is running at port ${port}`));
