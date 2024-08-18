f2d9c69d77756a575e3121626e1d2336ff7bbf54
juanm98 <juanmgarcia098@gmail.com>
2024-08-17 21:27:30 -0400

    Esctructure for Object

--- index.ts
+++ index.ts
@@ -5,5 +5,12 @@ const port = 3000;
 
 app.use(express.json());
 
+type Contact {
+    id: number;
+    fullName: string;
+    phoneNumber: string;
+    email: string
+};
+
 
 app.listen(port, () => console.log(`This server is running at port ${port}`));
