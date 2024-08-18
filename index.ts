2aaf6a9ea50624903d13d2a37f7a025bd8b9a36f
juanm98 <juanmgarcia098@gmail.com>
2024-08-17 21:24:03 -0400

    Add middleware

--- index.ts
+++ index.ts
@@ -3,4 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+
 app.listen(port, () => console.log(`This server is running at port ${port}`));
